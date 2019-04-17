56d9e01da93c45b01c0b746057fab72769cbacf2
Jack Maddox <jackdmaddox@gmail.com>
2019-04-17 11:05:06 -0400

    Moved variables out of function and made them global, also committed class follow along

---
+++ madlibsfunctionclassfollo.py
@@ -0,0 +1,4 @@
+person = input("What is your name? ")
+subject = input("What is your favorite subject? ")
+
+print ("Your name is %s and your favorite subject is %s" % (person, subject))
--- madlibsfunctions.py
+++ madlibsfunctions.py
@@ -1,3 +1,7 @@
+
+"""
+Commenting out because the variables were stored inside of the function instead of being outside and global
+
 def madlibsfunction():
     celebAction = input('Enter a verb...').lower()
 
@@ -12,5 +16,17 @@ def madlibsfunction():
     numberOfAnimals = int(input('How many of them are there?'))
 
     print ("%s " "%s " "are" " %s" " %s " "while %s %ss" % (numberOfAnimals, animal, animalVerb, celebrity, gender, celebAction))
+"""
 
-madlibsfunction()
+celebAction = input('Enter a verb...').lower()
+celebrity = input('Please enter the name of a celebrity...').title()
+gender = input('is that a he, she or they?').lower()
+animal = input('Please enter a type of animal...').lower()
+animalVerb = input('What is the animal doing...').lower()
+numberOfAnimals = int(input('How many of them are there?'))
+
+def madlibsfunction(celebAction, celebrity, gender, animal, animalVerb, numberOfAnimals):
+    return print ("%s " "%s " "are" " %s" " %s " "while %s %ss" % (numberOfAnimals, animal, animalVerb, celebrity, gender, celebAction))
+
+
+madlibsfunction(celebAction, celebrity, gender, animal, animalVerb, numberOfAnimals)
