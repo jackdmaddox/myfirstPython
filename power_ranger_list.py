4a5eb471ac8330a0bb9f8debdacab4ed3f7ae8cb
Jack Maddox <jackdmaddox@gmail.com>
2019-04-18 22:37:41 -0400

    Completed all day 4 exercises

--- power_ranger_list.py
+++ power_ranger_list.py
@@ -1,13 +1,16 @@
 power_rangers = []
 
-print(power_rangers)
-
 power_rangers.append("Jason")
 power_rangers.append("Trini")
 power_rangers.append("Billy")
 power_rangers.append("Zack")
 power_rangers.append("Kim")
 
-print(power_rangers)
+i = 0
 
-print(power_rangers[0])
+while i < len(power_rangers):
+    print(power_rangers[i])
+    i = i + 1
+
+
+
--- string_exercises_day4.py
+++ string_exercises_day4.py
@@ -61,10 +61,75 @@
 #         matrix_result.append(matrix_one[outerlist][innernumber] + matrix_two[outerlist][innernumber])
 # print(matrix_result)
 
-#De-Dup
-first_list = [1, 2, 2, 2, 3, 3, 4]
-deduped_list = []
-for number in first_list:
-    if number not in deduped_list:
-        deduped_list.append(number)
-print(deduped_list)
+# #De-Dup
+# first_list = [1, 2, 2, 2, 3, 3, 4]
+# deduped_list = []
+# for number in first_list:
+#     if number not in deduped_list:
+#         deduped_list.append(number)
+# print(deduped_list)
+
+#Uppercase a string
+# mystring = "mystring"
+# print(mystring.upper())
+
+#Capitalize a string
+# mystring = "mystring"
+# print(mystring.title())
+
+#Reverse a String
+# mystring = "mystring"
+# reverse_string = ""
+# length = len(mystring) - 1
+# while length >= 0:
+#     reverse_string = reverse_string + mystring[length]
+#     length = length - 1
+# print(reverse_string)
+
+#Leetspeak
+# original_text = "I am an elite hacker and this is Leet speak."
+# print(original_text)
+# char_1 = "a"
+# char_2 = "e"
+# char_3 = "g"
+# char_4 = "i"
+# char_5 = "o"
+# char_6 = "s"
+# char_7 = "t"
+# for a in char_1:
+#     original_text = original_text.replace(a, "4")
+# for e in char_2:
+#     original_text = original_text.replace(e, "3")
+# for g in char_3:
+#     original_text = original_text.replace(g, "6")
+# for i in char_4:
+#     original_text = original_text.replace(i, "1")
+# for o in char_5:
+#     original_text = original_text.replace(o, "0")
+# for s in char_6:
+#     original_text = original_text.replace(s, "5")
+# for t in char_7:
+#     original_text = original_text.replace(t, "7")
+
+# print(original_text)
+
+#Long-long Vowels
+# original_word = input("enter word with long vowels ")
+
+# if "ee" in original_word:    
+#     original_word = original_word.replace("ee", "eeeee")
+#     print(original_word)
+    
+# elif "oo" in original_word:
+#     original_word = original_word.replace("oo", "ooooo")
+#     print(original_word)
+
+# else:
+#     print("you bastard, that's not a long vowel")
+
+#Caeser Cipher - You Must Unlearn What You Have Learned
+# original = input("enter the word you want translating ")
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+# caeser_alphabet = "nopqrstuvwxyzabcdefghijklm"
+# translation = str.maketrans(alphabet, caeser_alphabet)
+# print(original.translate(translation))
